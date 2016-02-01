3a6959fde9ea3d26329efd69fe3cd0097de36519
Will Moore <william.moore@opower.com>
2016-02-01 10:42:45 -0500

    Add highcharts!

---
+++ public/javascripts/charts.js
@@ -0,0 +1,60 @@
+$(function () {
+    // TODO promises
+    $.get('/api/v1/person', function(people) {
+        $.get('/api/v1/iteration', function(iterations) {
+            $.get('/api/v1/results', function(results) {
+
+                $('#container').highcharts({
+                    title: {
+                        text: 'Average Engagement Over Time',
+                        x: -20 //center
+                    },
+                    xAxis: {
+                        categories: _.map(iterations, function(iteration) {
+                            return iteration.name;
+                        })
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Avg. Engagement'
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                    },
+                    tooltip: {
+                        valueSuffix: ' Engagenent'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: _.map(people, function(person) {
+                        return { 'name' : person.name, 
+                            'data': _.chain(results)
+                                .filter(function(result) {
+                                    return result.user_id == person.id;
+                                })
+                                .map(function(result) {
+                                    return _.round(_.mean([ 
+                                        result.fit,
+                                        result.proud,
+                                        result.excited,
+                                        result.meaningful
+                                    ]));
+                                })
+                                .value()
+                        };
+                    })
+                });
+
+                $('#spinner').hide();
+                
+            });
+        });
+    });
+});
