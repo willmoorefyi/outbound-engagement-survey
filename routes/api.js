58b10b1c0fd6035a056245237b4b6d253c14c151
Will Moore <william.moore@opower.com>
2016-02-01 00:32:21 -0500

    add iteration endpoint

--- routes/api.js
+++ routes/api.js
@@ -7,13 +7,13 @@ var pg = require('pg');
 router.get('/', function(req, res, next) {
     console.log('Reading Database URL: ' + process.env.DATABASE_URL);
 
-    runSql('SELECT * FROM results;', function(err, results) {
+    runSql('SELECT * FROM results;', null, function(err, results) {
         res.render('index', { title: 'API' });
     });
 });
 
 router.get('/person', function(req, res, next) {
-    runSql("SELECT * FROM person", function(err, results) {
+    runSql("SELECT * FROM person", null, function(err, results) {
         console.log('Got results: ' + results);
         res.status(200).send(results.rows);
     });
@@ -27,23 +27,48 @@ router.put('/person', function(req, res, next) {
         email + '" with name "' + name + '"');
 
     if(!email || !name) {
-        res.status(400).send( { error: 'Did not provide "email" and "name" '});
+        res.status(400).send( { error: 'Did not provide "email" and "name"'});
     }
 
-    runSql("INSERT INTO person (email, name) VALUES('" + email + "', '" + name + "');", function(err, results) {
+    runSql("INSERT INTO person (email, name) VALUES($1, $2);", [ email, name ], function(err, results) {
         res.status(200).send({ 'Status' : 'OK' });
     }, this);
 });
 
+router.get('/iteration', function(req, res, next) {
+    runSql("SELECT * FROM iteration", null, function(err, results) {
+        console.log('Got results: ' + results);
+        res.status(200).send(results.rows);
+    });
+});
+
+router.put('/iteration', function(req, res, next) {
+    var name = req.body.name;
+    var startDate = req.body.startDate;
+    var endDate = req.body.endDate;
+
+    console.log('Connecting to DB: "' + process.env.DATABASE_URL + '" to add iteration: "' + 
+        name + '" with start "' + startDate + '" and end "' + endDate + '"');
+
+    if(!name || !startDate || !endDate) {
+        res.status(400).send( { error: 'Did not provide "name", "startDate", and "endDate"'});
+    }
+
+    runSql("INSERT INTO iteration (name, date_retro_start, date_retro_end) VALUES($1, $2, $3);", 
+        [ name, new Date(startDate), new Date(endDate) ], function(err, results) {
+        res.status(200).send({ 'Status' : 'OK' });
+    });
+});
+
 // TODO add error callback
-function runSql(sql, callback, scope) {
+function runSql(sql, params, callback, scope) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         if (!client) {
             console.error('Could not connect to database');
             response.send({ error: 'could not connect to Database' });
         } else {
             console.log('Connected to DB');
-            client.query(sql, function(err, results) {
+            client.query(sql, params, function(err, results) {
                 done();
                 if (err) {
                     console.error('Error: ' + err);
