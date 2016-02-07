c9f7f228a9f6e3bacb28f05e1c28394990086692
Will Moore <william.moore@opower.com>
2016-02-06 21:52:22 -0500

    Convert ajax to promises, fixes #2

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,75 +1,74 @@
 $(function () {
-    // TODO promises
-    $.get('/api/v1/person', function(people) {
-        $.get('/api/v1/iteration', function(iterations) {
-            $.get('/api/v1/results', function(results) {
 
-                $('#container').highcharts({
-                    chart: {
-                        type: 'spline'
-                    },
-                    title: {
-                        text: 'Average Engagement Over Time',
-                        x: -20 //center
-                    },
-                    xAxis: {
-                        categories: _.map(iterations, function(iteration) {
-                            return iteration.name;
-                        })
-                    },
-                    yAxis: {
-                        title: {
-                            text: 'Avg. Engagement'
-                        },
-                        min: 0,
-                        max: 7,
-                        tickAmount: 8,
-                        plotLines: [{
-                            value: 0,
-                            width: 1,
-                            color: '#808080'
-                        }]
-                    },
-                    tooltip: {
-                        valueSuffix: ' Engagenent'
-                    },
-                    legend: {
-                        layout: 'vertical',
-                        align: 'right',
-                        verticalAlign: 'middle',
-                        borderWidth: 0
-                    },
-                    series: _.map(people, function(person) {
-                        // TODO this is horrible, clean it up!
-                        var my_results = _.filter(results, function(result) {
-                            return result.user_id == person.id;
-                        });
-
-                        var data = _.map(iterations, function(iteration) {
-
-                            var my_result = _.find(my_results, function(my_result) {
-                                return my_result.iteration_id == iteration.id;
-                            });
-
-                            if (my_result) { 
-                                return _.floor(_.mean([ 
-                                    my_result.fit,
-                                    my_result.proud,
-                                    my_result.excited,
-                                    my_result.meaningful
-                                ]));
-                            } else {
-                                return null;
-                            }
-                        });
-
-                        return { 'name' : person.email, 'data' : data };
-                    })
+    Promise.all([
+        $.get('/api/v1/person'),
+        $.get('/api/v1/iteration'),
+        $.get('/api/v1/results') ])
+    .spread(function(people, iterations, results) {
+        $('#container').highcharts({
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: 'Average Engagement Over Time',
+                x: -20 //center
+            },
+            xAxis: {
+                categories: _.map(iterations, function(iteration) {
+                    return iteration.name;
+                })
+            },
+            yAxis: {
+                title: {
+                    text: 'Avg. Engagement'
+                },
+                min: 0,
+                max: 7,
+                tickAmount: 8,
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: ' Engagenent'
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: _.map(people, function(person) {
+                // TODO this is horrible, clean it up!
+                var my_results = _.filter(results, function(result) {
+                    return result.user_id == person.id;
                 });
 
-                $('#spinner').hide();
-                
-            });
+                var data = _.map(iterations, function(iteration) {
+
+                    var my_result = _.find(my_results, function(my_result) {
+                        return my_result.iteration_id == iteration.id;
+                    });
+
+                    if (my_result) { 
+                        return _.floor(_.mean([ 
+                            my_result.fit,
+                            my_result.proud,
+                            my_result.excited,
+                            my_result.meaningful
+                        ]));
+                    } else {
+                        return null;
+                    }
+                });
+
+                return { 'name' : person.email, 'data' : data };
+            })
         });
+
+        $('#spinner').hide();    
     });
+
 });
