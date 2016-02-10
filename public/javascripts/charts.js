f4b7e1fb2d0b454dd13883a4363e62711b1894cb
Will Moore <william.moore@opower.com>
2016-02-10 09:23:28 -0500

    Refactor of client-side, added company graph
    Cleaned up code that calculated personal averages, refactored out common
    methods and cleaned up styling using less

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -4,77 +4,103 @@ $(function () {
         $.get('/api/v1/person'),
         $.get('/api/v1/iteration'),
         $.get('/api/v1/results') ])
-    .spread(function(people, iterations, results) {
-        $('#container').highcharts({
-            chart: {
-                type: 'spline'
-            },
-            title: {
-                text: 'Average Engagement Over Time',
-                x: -20 //center
-            },
-            xAxis: {
-                categories: _.map(iterations, function(iteration) {
-                    return iteration.name;
-                })
-            },
-            yAxis: {
-                title: {
-                    text: 'Avg. Engagement'
-                },
-                min: 0,
-                max: 7,
-                tickAmount: 8,
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                valueSuffix: ' Engagenent'
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle',
-                borderWidth: 0
-            },
-            series: _.map(people, function(person) {
+    .spread(function(all_people, all_iterations, all_results) {
+        var engagement_data = {};  
+        var company_data = {};
+        var iterations = [];
 
-                var name = _.chain(person.email.split('@')[0].split('\.'))
-                    .map(_.capitalize)
-                    .join(' ')
-                    .value()
-
-                // TODO this is horrible, clean it up!
-                var my_results = _.filter(results, function(result) {
-                    return result.user_id == person.id;
-                });
-
-                var data = _.map(iterations, function(iteration) {
-
-                    var my_result = _.find(my_results, function(my_result) {
-                        return my_result.iteration_id == iteration.id;
-                    });
-
-                    if (my_result) { 
-                        return _.floor(_.mean([ 
-                            my_result.fit,
-                            my_result.proud,
-                            my_result.excited,
-                            my_result.meaningful
-                        ]), 2);
-                    } else {
-                        return null;
-                    }
-                });
-
-                return { 'name' : name, 'data' : data };
-            })
+        _.each(all_people, function(person) {
+            var name =  _.map(person.email.split('@')[0].split('\.'), _.capitalize).join(' ');
+            engagement_data[person.id] = { 'name' : name, 'values' : {} };
+            company_data[person.id] = { 'name' : name, 'values' : {} };  
         });
 
-        $('#spinner').hide();    
-    });
+        _.each(all_results, function(result) {
+            iterations.push(result.iteration_id);
 
+            company_data[result.user_id]['values'][result.iteration_id] = result.company;
+            engagement_data[result.user_id]['values'][result.iteration_id] = _.floor(_.mean([ 
+                    result.fit,
+                    result.proud,
+                    result.excited,
+                    result.meaningful
+                ]), 2);
+        });
+
+        // use 10 most recent iterations with actual data
+        iterations = _.takeRight(_.uniq(iterations).sort(), 10);
+
+        var categories = _(all_iterations).filter(function(iteration) {
+            return iterations.indexOf(iteration.id) >= 0;
+        }).map('name').value();
+
+        highchartElem($('#engagement-table-container'), 
+            'Average Engagement Over Time',
+            categories, 
+            'Avg. Engagement',
+            ' Engagenent',
+            buildMapData(engagement_data, iterations));
+        highchartElem($('#company-table-container'), 
+            'Company Rating',
+            categories, 
+            'Rating',
+            ' Rating',
+            buildMapData(company_data, iterations));
+
+
+        $('#spinner').hide(); 
+    });
 });
+
+function buildMapData(all_data, iterations) {
+    return _.map(all_data, function(result) {
+        var name = result.name;
+        var data = _.fill(new Array(iterations.length), null);
+
+        _.forIn(result.values, function(value, iteration_id) {
+            var index = iterations.indexOf(parseInt(iteration_id));
+            if (index >= 0) {
+                data[index] = value;
+            }
+        });
+        return { 'name' : name, 'data' : data };
+    });
+}
+
+function highchartElem(elem, title_text, categories, y_axis_title, tooltip, data) {
+    elem.highcharts({
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: title_text,
+            x: -20 //center
+        },
+        xAxis: {
+            categories: categories
+        },
+        yAxis: {
+            title: {
+                text: y_axis_title
+            },
+            min: 0,
+            max: 7,
+            tickAmount: 8,
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: tooltip
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: data
+    });   
+}
