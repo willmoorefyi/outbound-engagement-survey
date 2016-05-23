de63ce12f9dc977893c337fc59365bdbc3cd95df
Will Moore <william.moore@opower.com>
2016-05-23 13:50:08 -0400

    Add team to people overview page

--- public/javascripts/people.js
+++ public/javascripts/people.js
@@ -1,12 +1,20 @@
 $(function () {
 
-    Promise.resolve($.get('/api/v1/person')).then(function(all_people) {
+    Promise.all([
+        $.get('/api/v1/person'),
+        $.get('/api/v1/team')
+    ]).spread(function(all_people, all_teams) {
+        var teams = _.keyBy(all_teams, 'id');
+        var people = _.map(all_people, function(person) {
+            return _.merge(person, { 'team': teams[person.team_id]['name'] });
+        });
         $('#people').DataTable( {
             data: all_people,
             'rowId': 'id',
             "columns": [
                 { "data": "email" },
                 { "data": "name" },
+                { "data": "team" }
             ]
         });
     });
